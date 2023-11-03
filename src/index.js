c8414210e5a0962ce3c7b00adc977a81f6d7b30b
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-11-03 04:46:00 +0100

    create image and font assets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import './style.css'
+import './style.css';
+import chef from './images/chef.png';
+import carne from './images/carne.png';
+import colorato from './images/colorato.png';
+import crema from './images/crema.png';
+import disgustoso from './images/disgustoso.png';
+import gamberi from './images/gamberi.png';
+import pepe from './images/pepe.png';
+import pomodoro from './images/pomodoro.png';
+import salsiccia from './images/salsiccia.png';
+
 
 console.log("working");
+
+const container = document.querySelector("#container");
+console.log(container);
+
+const chefImage = new Image();
+chefImage.src = chef;
+chefImage.alt = "A chef";
+
+container.appendChild(chefImage);
